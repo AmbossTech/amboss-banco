9a1817a476d635991625df1aec623b14aa26efe8
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-04-13 21:50:25 +0200

    chore: second commit

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  slug: "my-app",
+  name: "My App",
+
+  ios: {
+    bundleIdentifier: "com.anonymous.ambossbanco",
+  },
+});
