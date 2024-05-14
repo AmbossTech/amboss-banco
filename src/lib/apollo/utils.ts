dcbe740bf210702dcfb86a6c6adc3453dd60d707
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-05-14 10:45:30 +0200

    chore: small changes

--- src/lib/apollo/utils.ts
+++ src/lib/apollo/utils.ts
@@ -1,13 +1,17 @@
 import { Observable } from '@apollo/client';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const promiseToObservable = (promise: any) =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   new Observable((subscriber: any) => {
     promise.then(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (value: any) => {
         if (subscriber.closed) return;
         subscriber.next(value);
         subscriber.complete();
       },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       (err: any) => subscriber.error(err)
     );
   });
