0656f89b8696e917b55194a673521b74e854419c
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-06-12 15:35:46 -0500

    style: small changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,9 @@ import { LoginForm } from '@/components/LoginForm';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-2 2xl:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-2">
       <ExternalHeader />
-      <div className="flex w-full max-w-5xl items-center justify-center py-10 2xl:py-40">
+      <div className="flex w-full max-w-5xl items-center justify-center py-10">
         <LoginForm />
       </div>
     </main>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -3,9 +3,9 @@ import { SignUpForm } from '@/components/SignUpForm';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-2 2xl:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-start p-2">
       <ExternalHeader />
-      <div className="flex w-full max-w-5xl items-center justify-center py-10 2xl:py-40">
+      <div className="flex w-full max-w-5xl items-center justify-center py-10">
         <SignUpForm />
       </div>
     </main>
