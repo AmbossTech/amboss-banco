011101c44e7bfc202915cbe1de395b2976b80955
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-06-17 22:49:48 -0500

    refactor: wallet check

--- src/views/app/Dashboard.tsx
+++ src/views/app/Dashboard.tsx
@@ -2,39 +2,23 @@
 
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useLocalStorage } from 'usehooks-ts';
 
-import { useGetWalletQuery } from '@/graphql/queries/__generated__/wallet.generated';
+import {
+  useGetAllWalletsQuery,
+  useGetWalletQuery,
+} from '@/graphql/queries/__generated__/wallet.generated';
 import { LOCALSTORAGE_KEYS } from '@/utils/constants';
 import { ROUTES } from '@/utils/routes';
 
 import { WalletInfo } from '../wallet/Wallet';
 
-export const Dashboard = () => {
-  const { push } = useRouter();
-
-  const [value] = useLocalStorage(LOCALSTORAGE_KEYS.currentWalletId, '');
-
+const Wallet: FC<{ walletId: string }> = ({ walletId }) => {
   const { data, loading } = useGetWalletQuery({
-    variables: { id: value },
-    skip: !value,
+    variables: { id: walletId },
     errorPolicy: 'ignore',
   });
-
-  useEffect(() => {
-    if (!value) return;
-    if (loading) return;
-    if (!!data?.wallets.find_one.id) return;
-    localStorage.removeItem(LOCALSTORAGE_KEYS.currentWalletId);
-  }, [data, loading, value]);
-
-  useEffect(() => {
-    if (value) return;
-    if (loading) return;
-    if (!!data?.wallets.find_one.id) return;
-    push(ROUTES.setup.wallet.home);
-  }, [value, data, push, loading]);
 
   if (loading) {
     return (
@@ -44,13 +28,63 @@ export const Dashboard = () => {
     );
   }
 
-  if (!value || !data?.wallets.find_one.id) {
+  if (!data?.wallets.find_one.id) {
     return null;
+  }
+
+  return <WalletInfo id={walletId} />;
+};
+
+export const Dashboard = () => {
+  const { push } = useRouter();
+
+  const [checkingId, setCheckingId] = useState<boolean>(true);
+
+  const [value, setValue] = useLocalStorage(
+    LOCALSTORAGE_KEYS.currentWalletId,
+    ''
+  );
+
+  const { data, loading, error } = useGetAllWalletsQuery();
+
+  useEffect(() => {
+    if (loading) return;
+    if (error) return;
+
+    // User has no wallets for his account
+    if (!data?.wallets.find_many.length) {
+      localStorage.removeItem(LOCALSTORAGE_KEYS.currentWalletId);
+      push(ROUTES.setup.wallet.home);
+      return;
+    }
+
+    const savedWallets = data.wallets.find_many;
+
+    if (value) {
+      const wallet = savedWallets.find(w => w.id === value);
+
+      // User has a valid wallet id in localstorage
+      if (!!wallet) {
+        setCheckingId(false);
+        return;
+      }
+    }
+
+    setValue(savedWallets[0].id);
+    setCheckingId(false);
+  }, [data, loading, error, value, push, setValue]);
+
+  if (loading || checkingId) {
+    return (
+      <div className="flex w-full justify-center py-4">
+        <Loader2 className="size-4 animate-spin" />
+      </div>
+    );
   }
 
   return (
     <div className="py-4">
-      <WalletInfo id={value} />
+      <Wallet walletId={value} />
     </div>
   );
 };
