26d4f7b85f512ea1e71be0d079d17e2de26281d9
secondl1ght <secondl1ght@protonmail.com>
2024-07-10 14:02:44 -0600

    feat: dynamic external header links

--- src/components/header/ExternalHeader.tsx
+++ src/components/header/ExternalHeader.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 
+import { useUserQuery } from '@/graphql/queries/__generated__/user.generated';
 import { ROUTES } from '@/utils/routes';
 
 import { LanguageToggle } from '../toggle/LanguageToggle';
@@ -9,18 +10,28 @@ import { ThemeToggle } from '../toggle/ThemeToggle';
 import { Button } from '../ui/button';
 
 export const ExternalHeader = () => {
+  const { data, loading } = useUserQuery();
+
   return (
     <div className="mt-1 flex w-full max-w-5xl flex-wrap items-center justify-between text-sm">
       <Link href={ROUTES.home}>
         <p className="text-xl font-bold">Banco</p>
       </Link>
       <div className="flex items-center justify-center gap-2">
-        <Button variant="ghost" asChild>
-          <Link href={ROUTES.login}>Login</Link>
-        </Button>
-        <Button asChild>
-          <Link href={ROUTES.signup}>Sign Up</Link>
-        </Button>
+        {loading ? null : data?.user.id ? (
+          <Button asChild>
+            <Link href={ROUTES.dashboard}>Go To App</Link>
+          </Button>
+        ) : (
+          <>
+            <Button variant="ghost" asChild>
+              <Link href={ROUTES.login}>Login</Link>
+            </Button>
+            <Button asChild>
+              <Link href={ROUTES.signup}>Sign Up</Link>
+            </Button>
+          </>
+        )}
         <ThemeToggle />
         <LanguageToggle />
       </div>
