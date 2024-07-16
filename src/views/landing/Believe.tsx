943ccb1eb30bc9cc571a4af527ae7e282030fce3
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2024-07-16 11:54:01 -0600

    feat: banco home page (#19)

---
+++ src/views/landing/Believe.tsx
@@ -0,0 +1,68 @@
+import Image, { StaticImageData } from 'next/image';
+import { FC } from 'react';
+
+import groupPhone from '/public/images/group-phone.webp';
+import manPhone from '/public/images/man-phone.webp';
+import womanCard from '/public/images/woman-card.webp';
+import womanPhone from '/public/images/woman-phone.webp';
+
+const Card: FC<{ title: string; subtitle: string; image: StaticImageData }> = ({
+  title,
+  subtitle,
+  image,
+}) => {
+  return (
+    <div className="relative">
+      <Image
+        src={image}
+        alt="stock-photo"
+        className="h-[520px] w-full rounded-2xl object-cover brightness-75"
+      />
+
+      <div className="absolute bottom-10 left-6 right-6">
+        <h4 className="mb-2 text-4xl font-semibold text-white">{title}</h4>
+
+        <p className="text-2xl font-medium text-white/85">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export const Believe = () => {
+  return (
+    <section className="mt-28 w-full px-4 lg:px-14">
+      <div className="mb-6 flex w-full flex-col gap-4 lg:flex-row lg:items-center">
+        <h2 className="text-7xl font-semibold text-primary lg:w-1/2 lg:text-9xl 2xl:text-[200px]">
+          Banco
+        </h2>
+
+        <h3 className="max-w-2xl text-4xl font-semibold text-black/65 lg:w-1/2 lg:text-5xl">
+          Access global finance in four simple steps.
+        </h3>
+      </div>
+
+      <div className="grid w-full gap-4 lg:grid-cols-2">
+        <Card
+          title="1. Sign up"
+          subtitle="Create your account with just an email and a password."
+          image={manPhone}
+        />
+        <Card
+          title="2. Share your lightning address"
+          subtitle="Connect with friends and family."
+          image={groupPhone}
+        />
+        <Card
+          title="3. Get paid"
+          subtitle="Receive dollars or bitcoin in seconds."
+          image={womanPhone}
+        />
+        <Card
+          title="4. Start banco-ing"
+          subtitle="Enjoy banking without boundaries and full control."
+          image={womanCard}
+        />
+      </div>
+    </section>
+  );
+};
---
+++ src/views/landing/Hero.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+
+import bitcoin from '/public/icons/bitcoin.svg';
+import tether from '/public/icons/tether.svg';
+import usdc from '/public/icons/usdc.svg';
+import { ROUTES } from '@/utils/routes';
+
+import { Header } from './Header';
+
+export const Hero = () => {
+  const [email, setEmail] = useState('');
+
+  return (
+    <div className="relative min-h-dvh w-full rounded-b-[40px] bg-gradient-to-b from-[#FAF5FF] to-[#F3E9FE]">
+      <Header />
+
+      <section className="flex h-[calc(100dvh-80px)] w-full items-center justify-center px-4 lg:h-[calc(100dvh-90px)]">
+        <Image
+          src={bitcoin}
+          alt="bitcoin"
+          className="absolute right-0 top-[5%] w-24 lg:top-[15%] lg:w-auto"
+        />
+
+        <div>
+          <h1 className="mb-8 text-center text-6xl font-bold text-black lg:text-8xl">
+            Your Money, Your Way.
+          </h1>
+
+          <p className="mb-12 text-center text-lg font-semibold text-black/85 lg:text-xl">
+            Pay and get paid in dollars and bitcoin. Simple, secure and always
+            in your control.
+          </p>
+
+          <Image
+            src={tether}
+            alt="tether"
+            className="absolute left-0 top-[60%] w-32 lg:top-[40%] lg:w-auto"
+          />
+
+          <div className="relative mx-auto w-full lg:w-[640px]">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              className="mb-2 h-12 w-full rounded-lg border border-neutral-200 bg-white px-4 py-2 text-base text-black placeholder:text-black/45 lg:mb-0 lg:h-14 lg:pr-48"
+            />
+
+            <Link
+              href={`${ROUTES.signup}${email ? '?email=' + encodeURIComponent(email) : ''}`}
+              className="flex h-10 w-full items-center justify-center rounded-lg bg-neutral-950 px-4 py-3 text-base font-semibold text-white transition-colors hover:bg-neutral-950/90 lg:absolute lg:right-2 lg:top-1/2 lg:w-auto lg:-translate-y-1/2"
+            >
+              Get Started Today
+            </Link>
+          </div>
+        </div>
+
+        <Image
+          src={usdc}
+          alt="usdc"
+          className="absolute bottom-0 right-0 w-36 lg:right-[12.5%] lg:w-auto"
+        />
+      </section>
+    </div>
+  );
+};
