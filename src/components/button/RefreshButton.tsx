5e31b724760817fc0a61db727c860d6ee0412150
secondl1ght <secondl1ght@protonmail.com>
2024-10-02 18:11:27 -0600

    refactor: new transaction redesign view

---
+++ src/components/button/RefreshButton.tsx
@@ -0,0 +1,37 @@
+import { RefreshCw } from 'lucide-react';
+import { useLocalStorage } from 'usehooks-ts';
+
+import { useRefreshWalletMutation } from '@/graphql/mutations/__generated__/refreshWallet.generated';
+import { cn } from '@/utils/cn';
+import { LOCALSTORAGE_KEYS } from '@/utils/constants';
+
+import { IconButton } from '../ui/button-v2';
+import { useToast } from '../ui/use-toast';
+
+export const RefreshButton = () => {
+  const { toast } = useToast();
+
+  const [value] = useLocalStorage(LOCALSTORAGE_KEYS.currentWalletId, '');
+
+  const [refresh, { loading: refreshLoading }] = useRefreshWalletMutation({
+    variables: { input: { wallet_id: value } },
+    refetchQueries: ['getWallet'],
+    onCompleted: () => toast({ title: 'Wallet Refreshed!' }),
+    onError: () =>
+      toast({
+        variant: 'destructive',
+        title: 'Error refeshing wallet.',
+      }),
+  });
+
+  return (
+    <IconButton
+      icon={
+        <RefreshCw size={20} className={cn(refreshLoading && 'animate-spin')} />
+      }
+      onClick={() => refresh()}
+      disabled={refreshLoading || !value}
+      className="z-10"
+    />
+  );
+};
